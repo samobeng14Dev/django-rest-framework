8a6c3538f459c3e9861deb4055356e2d22a0d583
Samuel Obeng <obengsamuel270@gmail.com>
2025-02-18 14:07:07 +0000

    created api views

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,8 @@
-from . import views
 from django.urls import path
+from .views import homepage, list_posts, post_detail  
 
 urlpatterns = [
-    path('homepage/', views.homepage,name='homepage'),
+    path('homepage/', homepage, name='homepage'),
+    path('', list_posts, name='list_posts'),
+    path('<int:post_index>/', post_detail, name='post_detail'),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,38 @@
-from django.shortcuts import render
-from django.http import HttpResponse,JsonResponse
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 
-# Create your views here.
-def homepage(request:HttpRequest):
-    response={"message":"Hello World"}
-    return JsonResponse(data=response)
+
+posts = [
+    {"id": 1, "title": "Why is it difficult to learn programming?", "content": "This is the first post."},
+    {"id": 2, "title": "Learn JavaScript", "content": "This is the second post."},
+    {"id": 3, "title": "Learn python", "content": "This is the third post."},
+]
+
+
+
+
+@api_view(http_method_names=['GET', 'POST'])
+def homepage(request: Request):
+    if request.method == 'POST':
+        data = request.data
+        response = {"message": "Hello, World!", "data": data}
+        return Response(data=response, status=status.HTTP_201_CREATED)
+
+    response = {"message": "Hello, World!"}
+    return Response(data=response, status=status.HTTP_200_OK)
+
+@api_view(http_method_names=['GET'])
+def list_posts(request: Request):
+    return Response(data=posts, status=status.HTTP_200_OK)    
+
+@api_view(http_method_names=['GET'])
+def post_detail(request:Request, post_index:int):
+    post = posts[post_index]
+
+    if post:
+        return Response(data=post, status=status.HTTP_200_OK) 
+
+    return Response(data={"message": "Post not found"}, status=status.HTTP_404_NOT_FOUND)     
+  
