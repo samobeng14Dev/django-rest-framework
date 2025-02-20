41fce838c0a35e7d17214dd7267e8ea0acec3737
Samuel Obeng <obengsamuel270@gmail.com>
2025-02-20 22:51:44 +0000

    jwt pairs

---
+++ accounts/tokens.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import get_user_model
+
+# User = get_user_model()
+
+
+def create_jwt_pair_for_user(user: User):
+    refresh = RefreshToken.for_user(user)
+
+    tokens = {
+        "access": str(refresh.access_token),
+        "refresh": str(refresh)
+    }
+
+    return tokens
--- simpleblog/settings.py
+++ simpleblog/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
+
+from django.conf import settings
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -64,10 +67,21 @@ REST_FRAMEWORK={
     "DEFAULT_AUTHENTICATION_CLASSES":(
         "rest_framework.authentication.TokenAuthentication",
         "rest_framework.authentication.SessionAuthentication",
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_PERMISSION_CLASSES":(
         "rest_framework.permissions.IsAuthenticated", 
         )
+}
+
+SIMPLE_JWT={
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=2),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "SIGNING_KEY": settings.SECRET_KEY,
+    "AUTH_HEADER_TYPES": ("Bearer",),
+
+    # "Bearer <Token>"
+
 }
 
 TEMPLATES = [
